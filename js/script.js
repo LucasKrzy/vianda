2152c97f8cb93e13525305338f2b2acc4275f5f3
Lucas Krzyzanowski <lucaskrzy@gmail.com>
2023-12-10 00:26:58 -0300

    commit banner mobile

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 25) {
+        $('nav').addClass('scrolled');
+        $('nav-bar').addClass('scrolled');
+    } else {
+        $('nav').removeClass('scrolled');
+        $('nav-bar').removeClass('scrolled');
+    }
+});
+$(document).ready(function() {
+    $('.navbar-toggler').click(function() {
+        $('nav.navbar').addClass('scrolled');
+    });
+
+    $('#basic-navbar-nav').on('hidden.bs.collapse', function () {
+        $('nav.navbar').removeClass('scrolled');
+    });
+});
