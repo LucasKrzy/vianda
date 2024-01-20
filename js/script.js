623fe3d56fa86cc85af64778d4c87a1893fc7b33
Lucas Krzyzanowski <lucaskrzy@gmail.com>
2024-01-20 17:08:25 -0300

    atualização imagens

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
+var logo = $('.navbar-brand img');
+
+function trocarImagem() {
+    var srcAtual = logo.attr('src');
+    var srcAlternativo = logo.data('alt');
+    logo.attr('src', srcAlternativo);
+    logo.data('alt', srcAtual);
+}
+
 $(window).scroll(function() {
     if ($(document).scrollTop() > 25) {
         $('nav').addClass('scrolled');
         $('nav-bar').addClass('scrolled');
+        trocarImagem();
     } else {
         $('nav').removeClass('scrolled');
         $('nav-bar').removeClass('scrolled');
+        trocarImagem();
     }
 });
-/*$(document).ready(function() {
-    $('.navbar-toggler').click(function() {
-        $('nav.navbar').addClass('scrolled');
-    });
-
-    $('#basic-navbar-nav').on('hidden.bs.collapse', function () {
-        $('nav.navbar').removeClass('scrolled');
-    });
-});*/
